efbd5cafab629bdec58eaec642081c9c55cdb5ab
Li Jiangming <li-jiangming@users.noreply.github.com>
2023-08-09 17:00:59 +0800

    Update main.c

--- c/main.c
+++ c/main.c
@@ -5,21 +5,12 @@
  *      Author: ljm
  */
 
-#include "ab_queue.h"
-#include "ab_stack.h"
 #include "ab_assert.h"
 #include "ab_thread_pool.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-void apply(void **x, void *cl) {
-    assert(x && *x);
-
-    int *num = *x;
-    printf("num = %d\n", *num);
-}
 
 void task_func(void *data) {
     assert(data);
@@ -33,37 +24,6 @@ int main(int argc, char *argv[]) {
     int num3 = 3;
     int num4 = 4;
     int num5 = 5;
-
-    printf("queue**********************\n");
-
-    ab_queue_t queue = ab_queue_new();
-
-    ab_queue_push(queue, &num1);
-    ab_queue_push(queue, &num2);
-    ab_queue_push(queue, &num3);
-    ab_queue_push(queue, &num4);
-    ab_queue_push(queue, &num5);
-
-    ab_queue_map(queue, apply, NULL);
-    ab_queue_pop(queue);
-    ab_queue_map(queue, apply, NULL);
-
-    ab_queue_free(&queue);
-
-    printf("stack**********************\n");
-    ab_stack_t stack = ab_stack_new();
-
-    ab_stack_push(stack, &num1);
-    ab_stack_push(stack, &num2);
-    ab_stack_push(stack, &num3);
-    ab_stack_push(stack, &num4);
-    ab_stack_push(stack, &num5);
-
-    ab_stack_map(stack, apply, NULL);
-    ab_stack_pop(stack);
-    ab_stack_map(stack, apply, NULL);
-
-    ab_stack_free(&stack);
 
     printf("threadpool**********************\n");
     ab_thread_pool_t tp = ab_thread_pool_new(10);
